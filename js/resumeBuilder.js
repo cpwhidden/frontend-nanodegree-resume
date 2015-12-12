e1651a35d9b6a7f846c3f2c42ee4f753f07dad36
cpwhidden <Christopher.Whidden@gmail.com>
2015-12-11 20:27:54 -0600

    add: Added basic data structures for script

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,90 @@
 /*
 This is empty on purpose! Your code to build the resume will go here.
  */
+
+var bio = {
+	name : "Chris Whidden",
+	role : "Software Developer",
+	contactInfo : {
+		email : "Christopher.Whidden@gmail.com"
+	},
+	pictureURL : "images/Me.jpg",
+	welcomeMessage : "Thanks for taking the opportunity to know more about me!",
+	skills : ["Swift", "iOS", "Xcode", "HTML", "CSS"]
+};
+
+var work = {
+	jobs: [
+	{
+		"position" : "Independent Software Developer",
+		"employer" : "Self-employed",
+		"dates" : "2015-",
+		"city" : "The World"
+		"description" : "Independently pursuing iOS apps to be published in the App Store"
+	},
+	{
+		"position" : "Library Assistant/Database Administrator",
+		"employer" : "Rochester Public Library",
+		"dates" : "2007-2015",
+		"city" : "Rochester, MN"
+		"description" : "Developed and administered databases that improved library functions"
+	}
+	]
+};
+
+var education = {
+	"schools": [
+	{
+		"name": "St. John's University",
+		"location": "Collegeville, MN",
+		"degree": "Bachelor of Arts"
+		"major": "English Literature",
+		"minor": "Ancient Greek"
+		"url" : "http://www.csbsju.edu/"
+	}]
+	"onlineCourses" : [
+	{
+		"title" : "iOS Developer Nanodegree",
+		"school" : "Udacity",
+		"dates" : "May 2015 - August 2015",
+		"url" : "https://www.udacity.com/course/ios-developer-nanodegree--nd003"
+	},
+	{
+		"title" : "Certificate in Database Management",
+		"school" : "University of Washington",
+		"dates" : "November 2013 - May 2014",
+		"url" : "http://www.pce.uw.edu/certificates/database-management.html"
+	}
+	]
+}
+
+var projects = {
+	"project" : [
+	{
+		"title" : "Flash Time",
+		"dates" : "August 2015",
+		"description" : "iOS flash card app",
+		"images" : []
+	},
+	{
+		"title" : "My Portfolio",
+		"dates" : "December 2015",
+		"description" : "Display of my works",
+		"images" : []
+	}]
+}
+
+
+
+var formattedName = HTMLheaderName.replace("%data%", bio.name);
+var formattedRole = HTMLheaderRole.replace("%data%", bio.role);
+var formattedEmail = HTMLemail.replace("%data%", bio.contactInfo.email);
+var formattedPic = HTMLbioPic.replace("%data%", bio.pictureURL);
+var formattedWelcomeMsg = HTMLwelcomeMsg.replace("%data%", bio.welcomeMessage);
+var formattedSkills = HTMLskills.replace("%data%", bio.skills);
+var formattedPosition = HTMLworkTitle.replace("%data%", bio.work.position);
+var formattedEducation = HTMLschoolName.replace("%data%", bio.education.name);
+
+$("#header").prepend(formattedName + formattedRole + formattedPic + formattedEmail + formattedWelcomeMsg + formattedSkills);
+$("#main").prepend(formattedPosition);
+$("#main").prepend(formattedEducation);
